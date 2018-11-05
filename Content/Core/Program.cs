18cfcd45498e9c70d904c513496b193e6156e59f
Sindre Wilting <sindrewilting@gmail.com>
2018-11-05 14:36:14 +0100

    Make boilerplates ready for latest and greatest

--- Content/Core/Program.cs
+++ Content/Core/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Core
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Autofac.Extensions.DependencyInjection;
+using System.IO;
+
+namespace Core
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                
+                .ConfigureServices(services => services.AddAutofac())
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>();
     }
 }
