5727105f565362b7b94b4ef3b5834730c06e3117
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-03 13:33:57 +0100

    Removing babel-polyfill - should be in main.js for IE11

--- Content/Web/webpack.config.js
+++ Content/Web/webpack.config.js
@@ -8,8 +8,5 @@ process.env.DOLITTLE_COMPONENT_DIR = path.resolve('./Components');
 
 const config = require('@dolittle/build.aurelia/webpack.config.js');
 
-config.entry = {
-    app: ['babel-polyfill', 'aurelia-bootstrapper'],
-    vendor: ['bluebird']
-};
+
 module.exports = config;
